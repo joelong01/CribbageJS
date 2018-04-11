bb969944e314494c78d365a2e9a9442848c90b77
Joe Long <joelong@outlook.com>
2018-04-11 13:50:59 -0700

    added headers to enable the UI (https://cribbageui.azurewebsites.net)

--- Cribbage/server.js
+++ Cribbage/server.js
@@ -56,10 +56,19 @@ router.get('/card/:name', function (req, res)
 app.use(function (req, res, next)
 {
 
-    console.log("app.use called");
+    addAllowHeaders(res, 'http://localhost:3000');
+    addAllowHeaders(res, 'https://cribbageui.azurewebsites.net');
+
+    // Pass to next layer of middleware
+    next();
+});
+
+function addAllowHeaders(res, host)
+{
+    console.log("addAllowHeader for " + host);
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', host);
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -70,10 +79,7 @@ app.use(function (req, res, next)
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', "false");
-
-    // Pass to next layer of middleware
-    next();
-});
+}
 
 
 //
